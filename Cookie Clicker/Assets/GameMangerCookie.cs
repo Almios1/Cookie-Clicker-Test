29252051a3715f3e8c5fd2afdea34c121a0bfb6f
Almios1 <57901289+Almios1@users.noreply.github.com>
2020-10-26 23:39:34 +0100

    Add Click and Console

---
+++ Cookie Clicker/Assets/GameMangerCookie.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameMangerCookie : MonoBehaviour
+{
+
+    public Button counterClick;
+
+    int counter = 0;
+    void Start()
+    {
+        Button counter = counterClick.GetComponent<Button>();
+        counter.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+    }
+}
