ef8be85703428b1e6a180bd8c4495d7d1a2603b3
Almios1 <57901289+Almios1@users.noreply.github.com>
2020-10-27 21:16:16 +0100

    Add first buying Object

--- Cookie Clicker/Assets/GameMangerCookie.cs
+++ Cookie Clicker/Assets/GameMangerCookie.cs
@@ -6,25 +6,47 @@ public class GameMangerCookie : MonoBehaviour
 {
 
     public Button counterClick;
-
+    public Button oneHandClick;
 
     float counter = 0;
-    float clickAmount = 1.2F;
+    float clickAmount = 1F;
 
     [SerializeField]
     private Text counterText;
 
+    [SerializeField]
+    private GameObject oneHand;
+
     void Start()
     {
-        Button counter = counterClick.GetComponent<Button>();
-        counter.onClick.AddListener(TaskOnClick);
+        oneHand.gameObject.SetActive(false);
+        
+        
+
     }
 
-    void TaskOnClick()
+    public void TaskOnClick()
     {
         counter += clickAmount;
-        Debug.Log(counter);
-       
         counterText.text = ("Counter: " + counter.ToString("F2"));
     }
+    public void oneHand2()
+    {
+        counter = counter - 100;
+        clickAmount += 0.2F;
+        counterText.text = ("Counter: " + counter.ToString("F2"));
+    }
+
+    void Update()
+    {
+        if(counter >= 100)
+        {
+            oneHand.gameObject.SetActive(true);
+        }
+        else
+        {
+            oneHand.gameObject.SetActive(false);
+        }
+    }
+
 }
